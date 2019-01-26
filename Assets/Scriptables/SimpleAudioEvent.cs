b1e37422336d037a636bfcd4c14ea74dc32b8550
Tyson <tysonjfmoyes@gmail.com>
2019-01-26 17:54:50 -0500

    David code pt 1

---
+++ Assets/Scriptables/SimpleAudioEvent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+[CreateAssetMenu(menuName="Audio Events/Simple")]
+public class SimpleAudioEvent : AudioEvent {
+	public AudioClip[] clips;
+	public RangedFloat volume;
+
+	AudioClip selectClip;
+	float selectVolume;
+
+	[MinMaxRange(0,2)]
+	public RangedFloat pitch;
+
+
+	public override void Play(AudioSource source)
+	{
+		if (clips.Length == 0)return;
+
+		selectClip = clips [Random.Range (0, clips.Length)];
+		selectVolume = Random.Range (volume.minValue, volume.maxValue);
+		source.pitch = Random.Range (pitch.minValue, pitch.maxValue);
+		source.PlayOneShot (selectClip, selectVolume);
+
+	}
+}
---
+++ Assets/Scripts/PrimaryObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimaryObject : WorldObject {
+
+	public GameObject secondaryObject;
+	public GameObject spawnParticle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	
+
+	public override void Interact(){
+		Instantiate(secondaryObject, this.transform.position,Quaternion.identity);
+		Instantiate(spawnParticle, this.transform.position,Quaternion.identity);
+		Destroy(this);
+	}
+}
