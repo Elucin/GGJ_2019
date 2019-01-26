e8172786dacc10ba3910101eeea86391d6e33fbc
Tyson <tysonjfmoyes@gmail.com>
2019-01-25 23:18:06 -0500

    Object Classes

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : WorldObject {
+	private const float THROW_FORCE = 10.0f;
+	private bool isCollected = false;
+	public Collectable Collect(){
+		isCollected = true;
+		return this; 
+	}
+
+	public void Drop(){
+		isCollected = false;
+		rb.AddForce(Vector3.up * THROW_FORCE / 4f);
+	}
+
+	public void Throw(Vector3 direction){
+		isCollected = false;
+		rb.AddForce(direction * THROW_FORCE, ForceMode.Impulse);
+	}
+
+}
---
+++ Assets/Scripts/WorldObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldObject : MonoBehaviour {
+	private static Color HIGHLIGHT_COLOR = Color.green;
+	public static WorldObject[] lstWorldObjects;
+
+	//Highlight
+	public static WorldObject highlighted = null;
+	private bool isHighlighted = false;
+
+	//Components
+	protected Rigidbody rb; 
+	private Material material;
+
+	//For Testing
+	public bool doHighlight = false;
+	
+	void Start(){
+		material = GetComponent<MeshRenderer>().material;
+		material.EnableKeyword("_EmissionColor");
+	}
+
+	public void Highlight(){
+		highlighted = this;
+		isHighlighted = true;
+		material.SetColor ("_EmissionColor", HIGHLIGHT_COLOR);
+		return;
+	}
+
+	void Update(){
+		if(doHighlight){
+			doHighlight = false;
+			Highlight();
+		}
+
+		if(highlighted != this && isHighlighted){
+			isHighlighted = false;
+			material.SetColor ("_EmissionColor", Color.black);
+		}
+	}
+}
