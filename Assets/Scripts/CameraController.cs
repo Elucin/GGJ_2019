090435b7b53eded53415801e83a815c9386d0917
Tyson <tysonjfmoyes@gmail.com>
2019-01-26 15:09:34 -0500

    made the camera dynamic

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,19 +4,37 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    public Vector3 Distance;
+    public float shiftSpeed;
+    public Vector3 staticOffset;
 
+    private Vector3 dynamicOffset = Vector3.zero;
     private Transform player;
-	// Use this for initialization
 	void Start ()
     {
         player = GameObject.Find("Player").transform;
 	}
 	
-	// Update is called once per frame
 	void Update ()
     {
+        if (Input.GetAxis("Horizontal") > 0)
+            dynamicOffset += Vector3.right * shiftSpeed * Time.deltaTime;
+        if (Input.GetAxis("Horizontal") < 0)
+            dynamicOffset += Vector3.left * shiftSpeed * Time.deltaTime;
+        if (Input.GetAxis("Vertical") > 0)
+            dynamicOffset += Vector3.up * shiftSpeed * Time.deltaTime;
+        if (Input.GetAxis("Vertical") < 0)
+            dynamicOffset += Vector3.down * shiftSpeed * Time.deltaTime;
+
+        if (Input.GetAxis("Horizontal") == 0)
+            dynamicOffset = new Vector3(Mathf.MoveTowards(dynamicOffset.x, 0, shiftSpeed * Time.deltaTime) , dynamicOffset.y);
+        if (Input.GetAxis("Vertical") == 0)
+            dynamicOffset = new Vector3(dynamicOffset.x, Mathf.MoveTowards(dynamicOffset.y, 0, shiftSpeed * Time.deltaTime));
+
+
+        dynamicOffset = Vector3.ClampMagnitude(dynamicOffset, 2);
+
+
         transform.position = player.position;
-        transform.position = transform.TransformPoint(Distance);
+        transform.position = transform.TransformPoint(staticOffset + dynamicOffset);
 	}
 }
