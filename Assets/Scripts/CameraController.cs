b73dad5a0558c6f0e7233613638c15f7816beaf2
Tyson <tysonjfmoyes@gmail.com>
2019-01-26 13:52:47 -0500

    added basic camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Vector3 Distance;
+
+    private Transform player;
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.Find("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = player.position + Distance;
+	}
+}
