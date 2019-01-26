7e8ac4ae0e7a59d23a895c10da16efc6af86c14e
Tyson <tysonjfmoyes@gmail.com>
2019-01-26 16:11:02 -0500

    smothed camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,8 +7,10 @@ public class CameraController : MonoBehaviour {
     public float shiftSpeed;
     public Vector3 staticOffset;
 
-    private Vector3 dynamicOffset = Vector3.zero;
+    public Vector3 dynamicOffset = Vector3.zero;
     private Transform player;
+
+    private Vector2 currentVolocity;
 	void Start ()
     {
         player = GameObject.Find("Player").transform;
@@ -25,10 +27,17 @@ public class CameraController : MonoBehaviour {
         if (Input.GetAxis("Vertical") < 0)
             dynamicOffset += Vector3.down * shiftSpeed * Time.deltaTime;
 
+        /*
         if (Input.GetAxis("Horizontal") == 0)
             dynamicOffset = new Vector3(Mathf.MoveTowards(dynamicOffset.x, 0, shiftSpeed * Time.deltaTime) , dynamicOffset.y);
         if (Input.GetAxis("Vertical") == 0)
             dynamicOffset = new Vector3(dynamicOffset.x, Mathf.MoveTowards(dynamicOffset.y, 0, shiftSpeed * Time.deltaTime));
+        */
+
+        if (Input.GetAxis("Horizontal") == 0 && (dynamicOffset.x > 0.01f || dynamicOffset.x < -0.01f))
+            dynamicOffset = new Vector3(Mathf.SmoothDamp(dynamicOffset.x, 0, ref currentVolocity.x, 1 ), dynamicOffset.y);
+        if (Input.GetAxis("Vertical") == 0 && (dynamicOffset.y > 0.01f || dynamicOffset.y < -0.01f))
+            dynamicOffset = new Vector3(dynamicOffset.x, Mathf.SmoothDamp(dynamicOffset.y, 0, ref currentVolocity.y, 1));
 
 
         dynamicOffset = Vector3.ClampMagnitude(dynamicOffset, 2);
@@ -36,5 +45,27 @@ public class CameraController : MonoBehaviour {
 
         transform.position = player.position;
         transform.position = transform.TransformPoint(staticOffset + dynamicOffset);
-	}
+
+        /*
+        dynamicOffset = new Vector3(0, 0, staticOffset.z);
+
+        if (Input.GetAxis("Horizontal") > 0)
+            dynamicOffset += Vector3.right * 2;
+        if (Input.GetAxis("Horizontal") < 0)
+            dynamicOffset += Vector3.left * 2;
+        if (Input.GetAxis("Vertical") > 0)
+            dynamicOffset += Vector3.up * 2;
+        if (Input.GetAxis("Vertical") < 0)
+            dynamicOffset += Vector3.down * 2;
+
+        if (Input.GetAxis("Horizontal") == 0)
+            dynamicOffset.x = 0;
+        if (Input.GetAxis("Vertical") == 0)
+            dynamicOffset.y = 0;
+
+
+        transform.position = player.position;
+        transform.position = transform.TransformPoint(staticOffset + dynamicOffset);
+        */
+    }
 }
