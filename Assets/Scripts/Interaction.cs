8eaf877b4ddaa9a4b02f0cf9d34ff7c2e6ad7ea0
Tyson <tysonjfmoyes@gmail.com>
2019-01-27 01:47:27 -0500

    interact particle

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -7,6 +7,7 @@ public class Interaction : MonoBehaviour {
 	public static GameObject player;
 	public static Collectable held = null;
 	// Use this for initialization
+	public ParticleSystem interactParticle;
 	void Start () {
 		player = this.gameObject;
 	}
@@ -16,6 +17,9 @@ public class Interaction : MonoBehaviour {
 		HighlightClosest(WorldObject.lstWorldObjects);
 
 		if(Input.GetButtonDown("Interact")){
+			if(!interactParticle.isPlaying){
+				interactParticle.Play();
+			}
 			if(held != null){
 				held.Interact();
 				held = null;
