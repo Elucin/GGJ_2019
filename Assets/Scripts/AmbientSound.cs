38b23852626a927bbaf9ecc52f3f305215a8ce4b
Nathan <nathandubord@gmail.com>
2019-01-27 10:57:03 -0500

    ambientsound

---
+++ Assets/Scripts/AmbientSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+public class AmbientSound : MonoBehaviour {
+	public float avgWait = 5f;
+	public float diff = 2.5f;
+
+	float timer;
+	public AudioSource player;
+	// Use this for initialization
+	void Start () {
+		timer = avgWait + Random.Range(-diff,diff);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timer>0){
+			timer-=Time.deltaTime;
+		}else{
+			player.Play();
+			timer = avgWait + Random.Range(-diff,diff);
+		}
+	}
+}
