f85a6fda734db810532c04b76a01fbcf37dac29b
Tyson <tysonjfmoyes@gmail.com>
2019-01-26 23:07:41 -0500

    work on stuff

--- Assets/Scripts/PrimaryObject.cs
+++ Assets/Scripts/PrimaryObject.cs
@@ -7,6 +7,10 @@ public class PrimaryObject : WorldObject {
 	public GameObject secondaryObject;
 	public GameObject spawnParticle;
 
+	public GameObject newObject;
+
+	
+
 
 	
 	// Update is called once per frame
@@ -15,6 +19,9 @@ public class PrimaryObject : WorldObject {
 	public override void Interact(){
 		Instantiate(secondaryObject, this.transform.position,Quaternion.identity);
 		Instantiate(spawnParticle, this.transform.position,Quaternion.identity);
+		if (newObject !=null){
+			Instantiate(newObject, this.transform.position,Quaternion.identity);
+		}
 		Destroy(this.gameObject);
 	}
 }
