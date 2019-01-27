11f35b21b18e35dc8436c989688c45e6063403c0
Nathan <nathandubord@gmail.com>
2019-01-27 12:57:43 -0500

    sound scripts

---
+++ Assets/Scripts/SoundArea.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+public class SoundArea : MonoBehaviour {
+
+	public AudioSource player;
+	// Use this for initialization
+
+	bool inArea = false;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+
+	void OnTriggerEnter(Collider c){
+		if(c.name == "Player"){player.Play();}
+	}
+
+	void OnTriggerExit(Collider c){
+		if(c.name == "Player"){player.Stop();}
+	}
+}
