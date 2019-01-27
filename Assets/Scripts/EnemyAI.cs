220d5c80bb1338e7c3b8e6ddc467ad0355945ec2
Nathan <nathandubord@gmail.com>
2019-01-27 09:59:14 -0500

    enemy death particle

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAI : SimpleAI {
+
+	public GameObject deathParticle;
 	public void Kill(){
 		//Set the player to safe
 		GameManager.instance.SetStatus(GameManager.Status.SAFETY);
 		
+		Instantiate(deathParticle,transform.position,Quaternion.identity);
 		//Sounds + Particle Systems
 		Destroy(gameObject);
 	}
